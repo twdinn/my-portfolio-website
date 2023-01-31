de490ed0e7a5ff009cfa80fc42656161c7b824f9
twdinn <twdinn@gmail.com>
2023-01-31 10:38:22 -0330

    jan 31 10:38

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import HomePage from "./pages/HomePage";
 import ProjectsPage from "./pages/ProjectsPage";
