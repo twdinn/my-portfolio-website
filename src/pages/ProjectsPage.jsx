a221e46f0e59e56040c582a4ea84af3cd9262019
twdinn <twdinn@gmail.com>
2023-02-09 02:53:05 -0330

    Amazon-Clone

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -1,44 +1,53 @@
 import Project from "../components/Project";
+import { v4 as uuidv4 } from "uuid";
 
 const ProjectsPage = () => {
+  const id = uuidv4();
   const projects = [
     {
-      id: 1,
+      id: { id },
+      name: "Amazon Clone",
+      desc: "A clone of Amazon",
+      proLink: "https://twdinn.github.io/amazon/",
+      image: "./media/amazon-clone.jpeg",
+    },
+    {
+      id: { id },
       name: "Sensa Humour",
       desc: "A social media app where you post funny jokes",
       proLink: "https://twdinn.github.io/social-media-jokes-app/",
       image: "./media/sensa-humour.jpeg",
     },
     {
-      id: 2,
+      id: { id },
       name: "SuperHero Generator",
       desc: "Search for a SuperHero and get back a picture and some stats. You can also choose to display a random Hero",
       proLink: "https://twdinn.github.io/superhero-api-react/",
       image: "./media/superhero-generator.jpeg",
     },
     {
-      id: 3,
+      id: { id },
       name: "Weather App",
       desc: "Search a city and get the current forcast",
       proLink: "https://twdinn.github.io/react-weather-api/",
       image: "./media/weather-app.jpeg",
     },
     {
-      id: 4,
+      id: { id },
       name: "Tip Calculator",
       desc: "Enter bill amount, tip percentage and amount of people splitting the bill and get the total back ",
       proLink: "https://twdinn.github.io/react-tip-calculator/",
       image: "./media/tip-calculator.jpeg",
     },
     {
-      id: 5,
+      id: { id },
       name: "Rock Paper Scissors Game",
       desc: "Play Rock Paper Scissors against the computer",
       proLink: "https://twdinn.github.io/rock-paper-scissors/",
       image: "./media/rock-paper-scissors.jpeg",
     },
     {
-      id: 6,
+      id: { id },
       name: "Pokedex",
       desc: "A List of Pokemon",
       proLink: "https://twdinn.github.io/pokedex/",
